f556987579cd2f19d9ebe609aea5e1c7995c37f6
Hao Xu <hao.xu@linux.dev>
2024-12-09 19:38:00 +0800

    use env in config and functionalize example buy

--- jupiter_py/config.py
+++ jupiter_py/config.py
@@ -1,8 +1,9 @@
 from solana.rpc.api import Client
 from solders.keypair import Keypair #type: ignore
+import os
 
-PRIV_KEY = "base_58_priv_key_str"
-RPC = "rpc_url_here"
+PRIV_KEY = os.getenv('SOL_PRIV_KEY', '')
+RPC = os.getenv('SOL_RPC', '')
 
 client = Client(RPC)
 payer_keypair = Keypair.from_base58_string(PRIV_KEY)
--- jupiter_py/example_buy.py
+++ jupiter_py/example_buy.py
@@ -1,7 +1,4 @@
 from jupiter_py import buy
 
-# Buy Example
-token_address = "7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr" # POPCAT
-sol_in = .1
-slippage = 10
-buy(token_address, sol_in, slippage)
+def jupiter_buy(token_addr, sol_in, slippage):
+    buy(token_addr, sol_in, slippage)
